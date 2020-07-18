f50bb93adc10f673ddb95e8a194bc42ca2f02713
Weverton Castro <wvcastro42@gmail.com>
2020-07-18 00:52:20 -0300

    Retirados comentários

--- 108.py
+++ 108.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
 #INHERITANCE
-# class Animal():
-#
-#     def __init__(self):
-#         print("Animal Created")
-#
-#     def who_am_i(self):
-#         print("Animal")
-#
-#     def eat(self):
-#         print("Eating...")
-#
-#
-# class Dog(Animal):
-#
-#     def __init__(self):
-#         # Animal.__init__(self)
-#         print("Dog Created!")
-#
-#     def bark(self):
-#         print("Wooof")
-#
-#     def eat(self):
-#         print("Dog eating")
-#
-#
-# my_dog = Dog()
-# my_dog.who_am_i()
-# my_dog.eat()
-# my_dog.bark()
-#
+class Animal():
+
+    def __init__(self):
+        print("Animal Created")
+
+    def who_am_i(self):
+        print("Animal")
+
+    def eat(self):
+        print("Eating...")
+
+
+class Dog(Animal):
+
+    def __init__(self):
+        # Animal.__init__(self)
+        print("Dog Created!")
+
+    def bark(self):
+        print("Wooof")
+
+    def eat(self):
+        print("Dog eating")
+
+
+my_dog = Dog()
+my_dog.who_am_i()
+my_dog.eat()
+my_dog.bark()
+
 
 # SPECIAL METHODS
 class Book():
