c85c9940d86376d26a93bccaddd44e537586ba9d
Weverton Castro <wvcastro42@gmail.com>
2020-08-30 17:34:45 -0300

    Curso em vídeo - colors.py

---
+++ CursoEmVideo/colors.py
@@ -0,0 +1,6 @@
+n = 'José'
+i=25
+# print('Você se chama {n} e tem {i} anos de idade')
+# print('Você se chama () e tem () anos de idade'.format(n, i))
+print('Você se chama [] e tem [] anos de idade'.format(n, i))
+# print('Você se chama {n} e tem {i} anos de idade'.format(n, i))
