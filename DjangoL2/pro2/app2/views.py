ffc9010816bc719da9b468e79a81efa0e4c57304
Weverton Castro <wvcastro42@gmail.com>
2020-09-10 17:24:06 -0300

    Class n. 140

--- DjangoL2/pro2/app2/views.py
+++ DjangoL2/pro2/app2/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
-from app2.models import User
+from app2.forms import NewUserForm
 
 def index(request):
     return render(request, 'app2/index.html')
 
 def users(request):
 
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users':user_list}
-    return render(request, 'app2/users.html', context=user_dict)
+	form = NewUserForm()
+
+	if request.method == 'POST':
+		form = NewUserForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print('ERROR FORM INVALID')
+
+	return render(request, 'app2/users.html', {'form': form})
