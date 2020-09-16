6a3a500cf2338a1f1379db016d98e696862a411d
Weverton Castro <wvcastro42@gmail.com>
2020-09-16 15:20:24 -0300

    DjangoL4 Lessons

---
+++ DjangoL4/learning_templates/basic_app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from basic_app import views
+
+#Template tagging
+app_name = 'basic_app'
+
+urlpatterns = [
+	path('relative/', views.relative, name='relative'),
+	path('other/', views.other, name='other'),
+]
